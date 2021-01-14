8414cb210afa78a41c0e15651fe81963a03df970
许继元 <490601115@qq.com>
2021-01-14 16:31:52 +0800

    perf: code formatting of AQueue

--- Queue/AQueue/AQueue.h
+++ Queue/AQueue/AQueue.h
@@ -1,48 +1,39 @@
-/***************************************************************************************
- *    File Name                :    AQueue.h
- *    CopyRight                :
- *
- *    SYSTEM                    :   Windows 10
- *    Create Data                :    2020.4.2
- *    Author/Corportation        :   Giyn
- *
- *
- *--------------------------------Revision History--------------------------------------
- *    No    version        Data            Revised By            Item            Description
- *
- *
- ***************************************************************************************/
+/**
+ * @filename AQueue.h
+ * @description AQueue header file
+ * @author 许继元
+ * @date 2020/4/24
+ */
 
 /**************************************************************
  *    Multi-Include-Prevent Section
  **************************************************************/
+
 #ifndef AQUEUE_H_INCLUDED
 #define AQUEUE_H_INCLUDED
 
 /**************************************************************
  *    Macro Define Section
  **************************************************************/
+
 #define MAXQUEUE 10
 
 /**************************************************************
  *    Struct Define Section
  **************************************************************/
-//循环队列结构
 
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct Aqueue
-{
+typedef struct Aqueue {
     void *data[MAXQUEUE]; //数据域
     int front;
     int rear;
     size_t length; //队列长度
 } AQueue;
 
-typedef enum
-{
-    FALSE=0, TRUE=1
+typedef enum {
+    FALSE = 0, TRUE = 1
 } Status;
 
 char type;
@@ -54,119 +45,107 @@ char datatype[MAXQUEUE];
 
 /**
  *  @name        : void InitAQueue(AQueue *Q)
- *    @description : 初始化队列
- *    @param         Q 队列指针Q
+ *  @description : 初始化队列
+ *  @param       : Q - 队列指针
  *  @notice      : None
  */
 void InitAQueue(AQueue *Q);
 
-
 /**
- *  @name        : void DestoryAQueue(AQueue *Q)
- *    @description : 销毁队列
- *    @param         Q 队列指针Q
+ *  @name        : void DestroyAQueue(AQueue *Q)
+ *  @description : 销毁队列
+ *  @param       : Q - 队列指针
  *  @notice      : None
  */
-void DestoryAQueue(AQueue *Q);
-
-
+void DestroyAQueue(AQueue *Q);
 
 /**
  *  @name        : Status IsFullAQueue(const AQueue *Q)
- *    @description : 检查队列是否已满
- *    @param         Q 队列指针Q
- *    @return         : 满-TRUE; 未满-FALSE
+ *  @description : 检查队列是否已满
+ *  @param       : Q - 队列指针
+ *  @return      : 满-TRUE; 未满-FALSE
  *  @notice      : None
  */
 Status IsFullAQueue(const AQueue *Q);
 
 /**
  *  @name        : Status IsEmptyAQueue(const AQueue *Q)
- *    @description : 检查队列是否为空
- *    @param         Q 队列指针Q
- *    @return         : 空-TRUE; 未空-FALSE
+ *  @description : 检查队列是否为空
+ *  @param       : Q - 队列指针Q
+ *  @return      : 空-TRUE; 未空-FALSE
  *  @notice      : None
  */
 Status IsEmptyAQueue(const AQueue *Q);
 
-
-
 /**
  *  @name        : Status GetHeadAQueue(AQueue *Q, void *e)
- *    @description : 查看队头元素
- *    @param         Q 队列指针Q，存放元素e
- *    @return         : 成功-TRUE; 失败-FALSE
+ *  @description : 查看队头元素
+ *  @param       : Q - 队列指针
+ *                 e - 存放元素
+ *  @return      : 成功-TRUE; 失败-FALSE
  *  @notice      : 队列是否空
  */
 Status GetHeadAQueue(AQueue *Q, void *e);
 
-
-
 /**
  *  @name        : int LengthAQueue(AQueue *Q)
- *    @description : 确定队列长度
- *    @param         Q 队列指针Q
- *    @return         : 队列长度count
+ *  @description : 确定队列长度
+ *  @param       : Q - 队列指针
+ *  @return      : 队列长度count
  *  @notice      : None
  */
 int LengthAQueue(AQueue *Q);
 
-
-
 /**
  *  @name        : Status EnAQueue(AQueue *Q, void *data)
- *    @description : 入队操作
- *    @param         Q 队列指针Q,入队数据指针data
- *    @return         : 成功-TRUE; 失败-FALSE
+ *  @description : 入队操作
+ *  @param       : Q - 队列指针
+ *                 data - 入队数据指针
+ *  @return      : 成功-TRUE; 失败-FALSE
  *  @notice      : 队列是否满了或为空
  */
 Status EnAQueue(AQueue *Q, void *data);
 
-
-
 /**
  *  @name        : Status DeAQueue(AQueue *Q)
- *    @description : 出队操作
- *    @param         Q 队列指针Q
- *    @return         : 成功-TRUE; 失败-FALSE
+ *  @description : 出队操作
+ *  @param       : Q - 队列指针
+ *  @return      : 成功-TRUE; 失败-FALSE
  *  @notice      : None
  */
 Status DeAQueue(AQueue *Q);
 
-
-
 /**
  *  @name        : void ClearAQueue(Queue *Q)
- *    @description : 清空队列
- *    @param         Q 队列指针Q
+ *  @description : 清空队列
+ *  @param       : Q - 队列指针
  *  @notice      : None
  */
 void ClearAQueue(AQueue *Q);
 
-
-
 /**
  *  @name        : Status TraverseAQueue(const AQueue *Q, void (*foo)(void *q))
- *    @description : 遍历函数操作
- *    @param         Q 队列指针Q，操作函数指针foo
- *    @return         : None
+ *  @description : 遍历函数操作
+ *  @param       : Q - 队列指针
+ *                 foo - 操作函数指针
+ *  @return      : None
  *  @notice      : None
  */
 Status TraverseAQueue(const AQueue *Q, void(*foo)(void *q));
 
 /**
  *  @name        : void APrint(void *q)
- *    @description : 操作函数
- *    @param         q 指针q
+ *  @description : 操作函数
+ *  @param       : q - 指针
  *  @notice      : None
  */
 void APrint(void *q);
 
 /**
- *  @name        : InputNumber
- *	@description : Get the correct integer input
- *	@param		 : none
- *	@return		 : int
+ *  @name        : int InputNumber()
+ *	@description : Check input number
+ *	@param		 : None
+ *	@return		 : Integer
  *  @notice      : None
  */
 int InputNumber();
@@ -175,7 +154,7 @@ int InputNumber();
  *  @name        : InputType
  *	@description : Get the data type
  *	@param		 : none
- *	@return		 : int
+ *	@return		 : Integer
  *  @notice      : None
  */
 int InputType();
@@ -183,8 +162,8 @@ int InputType();
 /**
  *  @name        : InputData
  *	@description : Get the data
- *	@param		 : none
- *	@return		 : none
+ *	@param		 : None
+ *	@return		 : None
  *  @notice      : None
  */
 void InputData(void *data);
@@ -192,5 +171,4 @@ void InputData(void *data);
 /**************************************************************
  *    End-Multi-Include-Prevent Section
  **************************************************************/
-#endif // AQUEUE_H_INCLUDED
-
+#endif
