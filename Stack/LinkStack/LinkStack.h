1103c226a0a980f53c3227d6f422706eb5b88f00
许继元 <490601115@qq.com>
2021-01-14 14:17:58 +0800

    perf: code formatting of LinkStack

--- Stack/LinkStack/LinkStack.h
+++ Stack/LinkStack/LinkStack.h
@@ -1,36 +1,39 @@
+/**
+ * @filename LinkStack.h
+ * @description LinkStack header file
+ * @author 许继元
+ * @date 2020/4/24
+ */
+
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 
-typedef enum Status
-{
+typedef enum Status {
     ERROR = 0,
-	SUCCESS = 1
+    SUCCESS = 1
 } Status;
 
 typedef int ElemType;
 
-typedef  struct StackNode
-{
-	ElemType data;
-	struct StackNode *next;
+typedef struct StackNode {
+    ElemType data;
+    struct StackNode *next;
 } StackNode, *LinkStackPtr;
 
-typedef  struct  LinkStack
-{
-	LinkStackPtr top;
-	int	count;
+typedef struct LinkStack {
+    LinkStackPtr top;
+    int count;
 } LinkStack;
 
-// LinkStack.
 Status initLStack(LinkStack *s); // 初始化栈
 Status isEmptyLStack(LinkStack *s); // 判断栈是否为空
-Status getTopLStack(LinkStack *s,ElemType *e); // 得到栈顶元素
+Status getTopLStack(LinkStack *s, ElemType *e); // 得到栈顶元素
 Status clearLStack(LinkStack *s); // 清空栈
 Status destroyLStack(LinkStack *s); // 销毁栈
-Status LStackLength(LinkStack *s,int *length); // 检测栈长度
-Status pushLStack(LinkStack *s,ElemType data); // 入栈
-Status popLStack(LinkStack *s,ElemType *data); // 出栈
-Status printStack(LinkStack * s); // 实时打印链栈
+Status LStackLength(LinkStack *s, int *length); // 检测栈长度
+Status pushLStack(LinkStack *s, ElemType data); // 入栈
+Status popLStack(LinkStack *s, ElemType *data); // 出栈
+Status printStack(LinkStack *s); // 实时打印链栈
 int InputNumber(); // 输入整数检测
 
 #endif
--- Stack/LinkStack/main.c
+++ Stack/LinkStack/main.c
@@ -1,15 +1,20 @@
+/**
+ * @filename main.c
+ * @description LinkStack test file
+ * @author 许继元
+ * @date 2020/4/24
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "LinkStack.h"
 
-int main()
-{
+int main() {
     int flag = 0; // There is no stack.
-    LinkStack *head = (LinkStack*)malloc(sizeof(StackNode));
+    LinkStack *head = (LinkStack *) malloc(sizeof(StackNode));
     head->count = 0;
     head->top = NULL;
-    while(1)
-    {
+    while (1) {
         system("cls");
         printf("                 \n");
         printf("                 \n");
@@ -27,81 +32,47 @@ int main()
         printStack(head);
         printf("-----------------\n");
         printf("Input a choice you want:");
-        switch(InputNumber())
-        {
+        switch (InputNumber()) {
             case 1:
                 initLStack(head);
                 flag = 1;
                 break;
             case 2:
-                if(flag == 1)
-                {
+                if (flag == 1) {
                     printf("Input the data to be push:");
                     pushLStack(head, InputNumber());
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                } else printf("Now there is no stack.\n");
                 break;
             case 3:
-                if(flag == 1)
-                {
-                    ElemType *e = (ElemType*)malloc(sizeof(ElemType));
+                if (flag == 1) {
+                    ElemType *e = (ElemType *) malloc(sizeof(ElemType));
                     popLStack(head, e);
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                } else printf("Now there is no stack.\n");
                 break;
             case 4:
-                if(flag == 1)
-                {
-                    clearLStack(head);
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                if (flag == 1) clearLStack(head);
+                else printf("Now there is no stack.\n");
                 break;
             case 5:
-                if(flag == 1)
-                {
+                if (flag == 1) {
                     destroyLStack(head);
                     flag = 0;
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                } else printf("Now there is no stack.\n");
                 break;
-            case 6:
-                {
-                    int *length = (int*)malloc(sizeof(int));
-                    LStackLength(head, length);
-                }
+            case 6: {
+                int *length = (int *) malloc(sizeof(int));
+                LStackLength(head, length);
+            }
                 break;
             case 7:
-                if(flag == 1)
-                {
-                    isEmptyLStack(head);
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                if (flag == 1) isEmptyLStack(head);
+                else printf("Now there is no stack.\n");
                 break;
             case 8:
-                if(flag == 1)
-                {
-                    ElemType *e = (ElemType*)malloc(sizeof(ElemType));
+                if (flag == 1) {
+                    ElemType *e = (ElemType *) malloc(sizeof(ElemType));
                     getTopLStack(head, e);
-                }
-                else
-                {
-                    printf("Now there is no stack.\n");
-                }
+                } else printf("Now there is no stack.\n");
                 break;
             case 9:
                 printf("\nByeBye~");
@@ -112,5 +83,6 @@ int main()
         }
         system("pause");
     }
+
     return 0;
 }
